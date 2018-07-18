0873fccf97c8982107057159aa6fa27b21cce0d9
kungfooman <lama12345@gmail.com>
2018-07-18 04:42:07 +0200

    cleanup/restructure of Waypoints.js and add Pathfinder.findPath

--- codemp/javascript/Waypoints.js
+++ codemp/javascript/Waypoints.js
@@ -1,3 +1,26 @@
+Waypoint = function(origin) {
+	this.origin = origin	
+	this.neighbors = {};
+	this.id = -1;
+	
+}
+
+Waypoint.prototype.distance = function(to) {
+	var dx = to.origin.x - this.origin.x
+	var dy = to.origin.y - this.origin.y
+	var dz = to.origin.z - this.origin.z
+	var dist =  Math.sqrt(dx*dx + dy*dy + dz*dz)
+	//console.log(this.neighbors[to])
+	console.log("distance from '" + this.name + "' to '" + to.name + "' is: " + dist);
+	return dist
+}
+
+Waypoint.prototype.connectTo = function(other) {
+	//this.neighbors.push( other )
+	this.neighbors[other.name] = other
+}
+
+
 Pathfinder = function() {
 	this.waypoints = []
 	this.edges = []
@@ -6,24 +29,26 @@ Pathfinder = function() {
 }
 
 Pathfinder.prototype.add = function(origin) {
-	this.waypoints.push( origin )
-
+	var wp = new Waypoint(origin);
+	this.waypoints.push( wp );
+	wp.id = this.waypoints.length - 1;
+	wp.name = "wp_" + wp.id
 	
 	var model = spawnentity()
 	model.origin = origin.sub( Vec3(0,0,25) )
 	model.model = "models/items/forcegem.md3"
 	this.entities.push( model )
 
-	return this.waypoints.length - 1 // return the "id"
+	return wp.id
 }
 
 Pathfinder.prototype.saveToFile = function(filename) {
-	var content = "var waypoints = new Pathfinder();\n";
+	var content = "var pathfinder = new Pathfinder();\n";
 	for (var i=0; i<this.waypoints.length; i++) {
-		var wp = this.waypoints[i]
-		content += "waypoints.add( " + wp.toString() + " );\n"
+		var origin = this.waypoints[i].origin
+		content += "pathfinder.add( " + origin.toString() + " );\n"
 	}
-	content += "waypoints;" // last statement of eval(...) is returned
+	content += "pathfinder;" // last statement of eval(...) is returned
 	file_put_contents(filename, content)
 }
 
@@ -52,8 +77,8 @@ Pathfinder.prototype.spawnEdgeEnts = function() {
 		var edge_from = this.edges[i][0]
 		var edge_to   = this.edges[i][1]
 
-		var wp_from = this.waypoints[edge_from]
-		var wp_to   = this.waypoints[edge_to  ]
+		var wp_from = this.waypoints[edge_from].origin
+		var wp_to   = this.waypoints[edge_to  ].origin
 
 		var delta = wp_to.sub(wp_from);
 		var angles = vecToAngles(delta)
@@ -71,6 +96,9 @@ Pathfinder.prototype.spawnEdgeEnts = function() {
 Pathfinder.prototype.connectBoth = function(id_from, id_to) {
 	this.edges.push( [id_from, id_to   ] )
 	this.edges.push( [id_to  , id_from ] )
+	
+	this.waypoints[id_from].connectTo( this.waypoints[id_to  ] )
+	this.waypoints[id_to  ].connectTo( this.waypoints[id_from] )
 }
 
 Pathfinder.prototype.connectSingle = function(id_from, id_to) {
@@ -79,6 +107,12 @@ Pathfinder.prototype.connectSingle = function(id_from, id_to) {
 
 Pathfinder.prototype.setupAstar = function(id_from, id_to) {
 	this.astar = new Astar()
+}
+
+Pathfinder.prototype.findPath = function(id_from, id_to) {
+	var a = this.waypoints[id_from];
+	var b = this.waypoints[id_to  ];
+	return this.astar.findPath(a, b)
 }
 	
 Pathfinder.LoadFromFile = function(filename) {
@@ -127,36 +161,13 @@ load_pathfinder = function() {
 	}
 
 
-	v = Vec3(11,22,33)
-	v.toString()
-	JSON.stringify(v)
-	JSON.stringify( Vec3(0,1,2) )
 
 	waypoints = new Pathfinder()
 	waypoints.add( player.origin )
 	waypoints.saveToFile("waypoints.txt")
 	waypoints.connect(0, 1)
 
-	Waypoint = function(origin) {
-		this.origin = origin	
-		this.neighbors = [];
-	}
 
-	Waypoint.prototype.distance = function(to) {
-		var dx = to.origin.x - this.origin.x
-		var dy = to.origin.y - this.origin.y
-		var dz = to.origin.z - this.origin.z
-		var dist =  Math.sqrt(dx*dx + dy*dy + dz*dz)
-		//console.log(this.neighbors[to])
-		console.log("distance from '" + this.name + "' to '" + to.name + "' is: " + dist);
-		return dist
-	}
-
-	Waypoint.prototype.connectTo = function(other) {
-		this.neighbors.push( other )
-	}
-
-	a = new Waypoint(player.origin)
 	b = new Waypoint(player.origin)
 	c = new Waypoint(player.origin)
 	d = new Waypoint(player.origin)
